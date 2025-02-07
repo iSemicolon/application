5e0e1b4bc9c4ca9b2e65bdc35b7e127863d0fb6d
iSemicolon <palash.samanta7497@gmail.com>
2025-02-07 23:13:40 +0530

    feature flag added

--- learningapp/obj/Debug/net8.0/learningapp.AssemblyInfo.cs
+++ learningapp/obj/Debug/net8.0/learningapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c56e57e418902ac1630c35ff1204d81af5f8e0eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df4fda7ca7d587e5c744efdddc0ad4638abe1152")]
 [assembly: System.Reflection.AssemblyProductAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learningapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
